78772959111c022649d3c3a2d8d34c5b25e380b2
amiramiour <amiouramirtahar@gmail.com>
2025-03-25 00:40:56 +0100

    activation de middleware

--- api/app.ts
+++ api/app.ts
@@ -1,10 +1,12 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './src/graphql/schemas/schema';
-import { resolvers } from './src/graphql/resolvers';
+import { resolvers } from './src/graphql/resolvers/index';
+import { createContext } from './src/graphql/context'; // 👈 Ajoute ça
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: createContext, //  Activer le middleware 
 });
 
 server.listen().then(({ url }) => {
