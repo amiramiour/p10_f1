0fcb2ecccf33ff563ffa97fb44e2cb1537c04f64
amiramiour <amiouramirtahar@gmail.com>
2025-03-25 00:41:11 +0100

    add middleware

--- api/src/graphql/resolvers/user/query.ts
+++ api/src/graphql/resolvers/user/query.ts
@@ -1,4 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import { GQLContext } from '../../context';
+
+const prisma = new PrismaClient();
+
 export const userQueries = {
-    hello: () => 'Hello from Apollo Server 🚀',
-  };
-  
+  hello: () => 'Hello from Apollo Server 🚀',
+
+  getMe: async (_: any, __: any, context: GQLContext) => {
+    if (!context.userId) {
+      throw new Error('Unauthorized');
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: context.userId },
+    });
+
+    return user;
+  },
+};
