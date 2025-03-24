4d7b740eda145fb495db5b6c0dba9a55310af728
amiramiour <amiouramirtahar@gmail.com>
2025-03-25 00:56:56 +0100

    ajout query et mutation league

--- api/src/graphql/resolvers/index.ts
+++ api/src/graphql/resolvers/index.ts
@@ -1,11 +1,15 @@
 import { userQueries } from './user/query';
 import { userMutations } from './user/mutation';
+import { leagueMutations } from './league/mutation';
+import { leagueQueries } from './league/query';
 
 export const resolvers = {
   Query: {
     ...userQueries,
+    ...leagueQueries,
   },
   Mutation: {
     ...userMutations,
+    ...leagueMutations,
   },
 };
