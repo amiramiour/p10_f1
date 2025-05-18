87e227735133b2b90fc76a00e8378a6750dd328e
amiramiour <amiouramirtahar@gmail.com>
2025-05-18 22:55:10 +0200

    bet query/mutation

--- api/src/graphql/resolvers/index.ts
+++ api/src/graphql/resolvers/index.ts
@@ -4,15 +4,22 @@ import { leagueMutations } from './league/mutation';
 import { leagueQueries } from './league/query';
 import { classementQueries } from './classement/query';
 import { gpQueries } from './gp/query';
+import { betQueries } from './bet/query';
+import { betMutations } from './bet/mutation';
+
 export const resolvers = {
   Query: {
     ...userQueries,
     ...leagueQueries,
     ...classementQueries,
     ...gpQueries,
+    ...betQueries,
+
   },
   Mutation: {
     ...userMutations,
     ...leagueMutations,
+    ...betMutations,
+
   },
 };
